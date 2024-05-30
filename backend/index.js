900e6b4024386e0d45503137db51b97a5af29595
ArulKumar8270 <arulkumar8270@gmail.com>
2024-05-31 01:36:13 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-var https = require('https');
+var https = require("https");
 
 const app = express();
 app.use(
@@ -26,6 +26,24 @@ app.use("/api/v1/company", companyRouter);
 app.use("/api/v1/jobs", jobsRouter);
 
 // handle error
+// app.use((error, req, res, next) => {
+//   console.log(error);
+//   const status = error?.status || 500;
+//   return res.status(status).json({
+//     success: false,
+//     message: error?.message || "Internal Server Error",
+//   });
+// });
+
+// var fs = require('fs');
+// var options = {
+//     key: fs.readFileSync('/home/rishijob/ssl/keys/98c9b_b7a2d_8f4aa6ded9a7b25a74794f650885bc26.key'),
+//     cert: fs.readFileSync('/home/rishijob/ssl/certs/rishijob_com_98c9b_b7a2d_1714435199_d2fc82f7f2f1ae93f9565bc2b27d10ae.crt'),
+// };
+
+// var listener = https.createServer(options, app).listen(5000, function () {
+//     console.log('Express HTTPS server listening on port ' + listener.address().port);
+// });
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error?.status || 500;
@@ -35,12 +53,4 @@ app.use((error, req, res, next) => {
   });
 });
 
-var fs = require('fs');
-var options = {
-    key: fs.readFileSync('/home/rishijob/ssl/keys/98c9b_b7a2d_8f4aa6ded9a7b25a74794f650885bc26.key'),
-    cert: fs.readFileSync('/home/rishijob/ssl/certs/rishijob_com_98c9b_b7a2d_1714435199_d2fc82f7f2f1ae93f9565bc2b27d10ae.crt'),
-};
-
-var listener = https.createServer(options, app).listen(5000, function () {
-    console.log('Express HTTPS server listening on port ' + listener.address().port);
-});
+app.listen(5000, () => console.log("Server is running on port 5000"));
