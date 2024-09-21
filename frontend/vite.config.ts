21c9aed34ab5534c18d448ad2743a69c05a7780e
ArulKumar8270 <arulkumar8270@gmail.com>
2024-09-21 15:47:30 +0530

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  server: {
+    host: '0.0.0.0',   // Bind to all network interfaces
+    port: 3000,        // Set your desired port (default is 5173)
+  }
+})
+  
